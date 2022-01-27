b7302785f6d3e0d9ed1a59e4c04f7847f44a3441
Christopher <ctijerina@live.com>
2022-01-26 20:42:11 -0600

    reloaded files, finished securing passwords and login process

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db, pass in process.env variables to represent your sensitive info
+// create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
